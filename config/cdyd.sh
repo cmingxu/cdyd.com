47036e3ebf04a992546863989b2f057093c85a01
Kevin.Xu <cmxu@kabam.com>
2014-10-20 23:47:31 +0800

    change

--- config/cdyd.sh
+++ config/cdyd.sh
@@ -1,18 +1,18 @@
 #!/bin/sh
 # rvm wrapper ruby-1.9.3-p194 bootup
 UNICORN=/home/ubuntu/.rvm/bin/bootup_unicorn
-CONFIG_FILE=/home/ubuntu/dswz/current/config/unicorn.rb
-APP_HOME=/home/ubuntu/dswz/current
+CONFIG_FILE=/home/ubuntu/cdyd/current/config/unicorn.rb
+APP_HOME=/home/ubuntu/cdyd/current
  
 case "$1" in
   start)
   $UNICORN -c $CONFIG_FILE -E production -D
   ;;
   stop)
-  kill -QUIT `cat /tmp/unicorn_dswz.pid`
+  kill -QUIT `cat /tmp/unicorn_cdyd.pid`
   ;;
   restart|force-reload)
-    kill -USR2 `cat /tmp/unicorn_dswz.pid`
+    kill -USR2 `cat /tmp/unicorn_cdyd.pid`
   ;;
   *)
    echo "Usage: $SCRIPTNAME {start|stop|restart|force-reload}" >&2
